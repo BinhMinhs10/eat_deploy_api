2b3e8e1b911c1f728ee6c7b127ebccb66602f66c
BinhMinhs10 <nguyenbinhminh07101997@gmail.com>
2020-12-27 13:05:29 +0700

    layout streamlit

--- first_app.py
+++ first_app.py
@@ -3,8 +3,32 @@ import numpy as np
 import pandas as pd
 
 st.title('My first app')
-st.write('Here first attempt at using data')
 
+# layout organize widgets
+add_selectbox = st.sidebar.selectbox(
+        'How would you like to be contacted?',
+        ('Email', 'Home page', 'Mobile phone')
+)
+
+add_slider = st.sidebar.slider(
+        'Select a range of values',
+        0.0, 100.0, (25.0, 75.0)
+        )
+
+
+left_column, right_column = st.beta_columns(2)
+# You can use a column just like st.sidebar:
+left_column.button('Press me!')
+
+# Or even better, call Streamlit functions inside a "with" block:
+with right_column:
+    chosen = st.radio(
+        'Sorting hat',
+        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
+    st.write(f"You are in {chosen} house!")
+
+
+st.write('Here first attempt at using data')
 df = pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
@@ -27,5 +51,30 @@ map_data = pd.DataFrame(
 
 st.map(map_data)
 
+# use a selecbox for options
+option =  st.selectbox(
+        'which number do you like best?',
+        df['first column'])
+'You selected: ', option
+
+
+# Layout app
+left_column, right_column = st.beta_columns(2)
+pressed = left_column.button('press me?')
+if pressed:
+    right_column.write('Hi i`m Minh')
+expander = st.beta_expander('FAQ')
+expander.write('Here you could put in some really')
+
+
+# Display status in real time
+import time
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i+1)
+    time.sleep(0.1)
 
 
